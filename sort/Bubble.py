2ad0c4fe11950a7d709e7c5357fb87ee94716e89
88888812 <ndp2012@yandex.ru>
2022-03-30 13:17:17 +0300

    Update README

--- sort/Bubble.py
+++
@@ -1,9 +0,0 @@
-import general_input
-def sort():
-    n,a= general_input.sort()
-
-    for i in range(n - 1):
-        if a[i] > a[i + 1]:
-            a[i], a[i + 1] = a[i + 1], a[i]
-
-    print(f'Результат:{a}')
